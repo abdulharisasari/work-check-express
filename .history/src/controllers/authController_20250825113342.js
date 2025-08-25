5fc898ea6630b536d994a253ca6e6299fa5ba1d4
haris <abdulharisasari4@gmail.com>
2025-08-25 14:21:57 +0700

    final

--- .history/src/controllers/authController_20250825113342.js
+++ .history/src/controllers/authController_20250825113342.js
@@ -2,39 +2,38 @@ const jwt = require('jsonwebtoken');
 const pool = require('../config/db');
 
 exports.login = async (req, res) => {
-    const { username, email, password } = req.body;
-
-    if ((!username && !email) || !password) {
-        return res.status(400).json({
-            code: 400,
-            message: 'Username/email dan password wajib diisi'
-        });
+    const { email, password } = req.body; 
+    if (!email || !password) {
+        return res.status(400).json({ code: 400, message: 'Email/Username dan password wajib diisi' });
     }
-
     try {
-        const loginParam = username || email;
+        
         const result = await pool.query(
-            'SELECT * FROM users WHERE username = $1 OR email = $1',
-            [loginParam]
+            'SELECT * FROM users WHERE email = $1 OR username = $1',
+            [email]  
         );
         if (result.rows.length === 0) {
             return res.status(404).json({ code: 404, message: 'User not found' });
         }
-
         const user = result.rows[0];
+
+        
         if (user.password !== password) {
             return res.status(401).json({ code: 401, message: 'Wrong password' });
         }
+
         const accessToken = jwt.sign(
             { id: user.id, email: user.email, username: user.username },
             process.env.JWT_SECRET,
             { expiresIn: '1h' }
         );
+
         const refreshToken = jwt.sign(
             { id: user.id, email: user.email, username: user.username },
             process.env.JWT_SECRET,
             { expiresIn: '7d' }
         );
+
         res.json({
             code: 200,
             message: 'Login successful',
@@ -43,7 +42,6 @@ exports.login = async (req, res) => {
             accesstoken: `Bearer ${accessToken}`,
             refreshtoken: `Bearer ${refreshToken}`
         });
-
     } catch (err) {
         console.error(err);
         res.status(500).json({ code: 500, message: 'Server error' });
